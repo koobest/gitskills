4ddb66c9dca0e021de465a8a33bce42c05640828
kooho <2229179028@qq.com>
2017-07-23 22:26:51 +0800

    motify

--- hello.c
+++ hello.c
@@ -1,23 +1,4 @@
 #include<stdio.h>
-int add(int a, int b)
-{
-    return a + b;
-}
-
-int sub(int a, int b)
-{
-    return a - b;
-}
-
-int mul(int a, int b)
-{
-    return a * b;
-}
-
-int div(int a, int b)
-{
-    return a / b;
-}
 
 int main(int argc, char** argv)
 {
